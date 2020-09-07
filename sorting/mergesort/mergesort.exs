8f3433f45c73e42fad7ba353785583ff78658381
czrpb <czrpb@yahoo.com>
2020-09-07 12:10:31 -0700

    updates

--- sorting/mergesort/mergesort.exs
+++ sorting/mergesort/mergesort.exs
@@ -1,28 +1,29 @@
 defmodule Merge do
-  defp _sort({[], [], m}) do
-    m
+  defp _sort([]) do
+    []
   end
 
-  defp _sort({[a], [], []}) do
-    [a]
+  defp _sort([l|[]]) do
+    l
   end
 
-  defp _sort({l, [], m}) do
-    _sort({l, m, []})
-  end
-  defp _sort({[], r, m}) do
-    _sort({r, m, []})
-  end
-
-  defp _sort({[a|l], [b|r], m}) do
+  defp _sort([[a], [b]]) do
     cond do
-      a<b -> _sort({[a|l], r, [b|m]})
-      b<=a -> _sort({l, [b|r], [a|m]})
+      a<b -> [a,b]
+      true -> [b,a]
     end
   end
 
+  defp _sort(list_of_lists) do
+    ">>> #{list_of_lists |> inspect}" |> IO.puts
+    list_of_lists
+    |> Enum.chunk_every(2)
+    |> Enum.map(&_sort/1)
+    |> _sort
+  end
+
   def sort(list) do
-    _sort({list, [], []})
+    _sort(Enum.chunk_every(list, 1))
   end
 end
 
@@ -34,6 +35,8 @@ end
 
 [1, 2, 3] |> Merge.sort |> inspect |> IO.puts
 [1, 3, 2] |> Merge.sort |> inspect |> IO.puts
+
+1/0
 
 [2, 1, 3] |> Merge.sort |> inspect |> IO.puts
 [2, 3, 1] |> Merge.sort |> inspect |> IO.puts
