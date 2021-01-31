9650eb3343d81161455b4423c8c10c83f04a9586
czrpb <czrpb00@gmail.com>
2021-01-31 15:00:22 -0800

    init for a balanced btree

---
+++ ds/bbtree/lib/bbtree.ex
@@ -0,0 +1,18 @@
+defmodule Bbtree do
+  @moduledoc """
+  Documentation for `Bbtree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bbtree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ds/bbtree/test/bbtree_test.exs
@@ -0,0 +1,8 @@
+defmodule BbtreeTest do
+  use ExUnit.Case
+  doctest Bbtree
+
+  test "greets the world" do
+    assert Bbtree.hello() == :world
+  end
+end
