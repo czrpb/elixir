1cb5d2ded9e66dcb964989a78633adaa154ff01f
czrpb <czrpb00@gmail.com>
2021-02-01 16:58:29 -0800

    updates; still need to fix counting for balance

--- ds/bbtree/lib/bbtree.ex
+++ ds/bbtree/lib/bbtree.ex
@@ -1,18 +1,74 @@
-defmodule Bbtree do
+defmodule BBTree do
   @moduledoc """
   Documentation for `Bbtree`.
   """
 
-  @doc """
-  Hello world.
+  def tree(value) do
+    {nil, {value, 1, 0}, nil}
+  end
+  def leaf?(tree) do
+    {left, _data, right} = tree
+    left == nil and right == nil
+  end
 
-  ## Examples
+  def value({_left, {value, _count, _bal}, _right}) do
+    value
+  end
+  def count({_left, {_value, count, _bal}, _right}) do
+    count
+  end
+  def bal({_left, {_value, _count, bal}, _right}) do
+    bal
+  end
+  def left({left, _data, _right}) do
+    left
+  end
+  def right({_left, _data, right}) do
+    right
+  end
 
-      iex> Bbtree.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def insert(newvalue, tree \\ nil) do
+    if tree == nil do
+      tree(newvalue)
+    else
+      {left, {value, count, bal}, right} = tree
+      newtree = cond do
+        newvalue == value ->
+          {left, {value, count+1, bal}, right}
+        left == nil and newvalue < value ->
+          subtree = tree(newvalue)
+          {subtree, {value, count, bal-1}, right}
+        right == nil and value < newvalue ->
+          subtree = tree(newvalue)
+          {left, {value, count, bal+1}, subtree}
+        newvalue < value ->
+          {insert(newvalue, left), {value, count, bal-1}, right}
+        value < newvalue ->
+          {left, {value, count, bal+1}, insert(newvalue, right)}
+      end
+      case newtree do
+        {_left, {_value, _count, -2}, nil} ->
+          balance_left(newtree)
+        {nil, {_value, _count, 2}, _right} ->
+          balance_right(newtree)
+        _ -> newtree
+      end
+    end
+  end
+  def balance_left(tree) do
+    case tree do
+      {{{nil, {llv, llc, 0}, nil}, {lv, lc, -1}, nil}, {v, c, -2}, nil} ->
+        {{nil, {llv, llc, 0}, nil}, {lv, lc, 0}, {nil, {v, c, 0}, nil}}
+      {{nil, {lv, lc, 1}, {nil, {lrv, lrc, 0}, nil}}, {v, c, -2}, nil} ->
+        {{nil, {lv, lc, 0}, nil}, {lrv, lrc, 0}, {nil, {v, c, 0}, nil}}
+    end
+  end
+  def balance_right(tree) do
+    case tree do
+      {nil, {v, c, 2}, {nil, {rv, rc, 1}, {nil, {rrv, rrc, 0}, nil}}} ->
+        {{nil, {v, c, 0}, nil}, {rv, rc, 0}, {nil, {rrv, rrc, 0}, nil}}
+      {nil, {v, c, 2}, {{nil, {rlv, rlc, 0}, nil}, {rv, rc, -1}, nil}} ->
+        {{nil, {v, c, 0}, nil}, {rlv, rlc, 0}, {nil, {rv, rc, 0}, nil}}
+    end
   end
 end
--- ds/bbtree/test/bbtree_test.exs
+++ ds/bbtree/test/bbtree_test.exs
@@ -1,8 +1,96 @@
-defmodule BbtreeTest do
+defmodule BBTreeTest do
   use ExUnit.Case
-  doctest Bbtree
+  doctest BBTree
 
-  test "greets the world" do
-    assert Bbtree.hello() == :world
+  test "new tree with value 4" do
+    tree = BBTree.tree(4)
+    assert tree == {nil, {4, 1, 0}, nil}
+    assert BBTree.leaf?(tree)
+    assert BBTree.value(tree) == 4
+    assert BBTree.count(tree) == 1
+    assert BBTree.bal(tree) == 0
+    assert BBTree.left(tree) == nil
+    assert BBTree.right(tree) == nil
+  end
+
+  test "tree with value 4 then insert 4" do
+    tree1 = 4 |> BBTree.insert(BBTree.tree(4))
+    tree2 = 4 |> BBTree.insert(BBTree.insert(4))
+    assert tree1 == {nil, {4, 2, 0}, nil}
+    assert tree1 == tree2
+  end
+
+  test "basic lesser and greater than inserts" do
+    root = BBTree.tree(5)
+    tree = 4 |> BBTree.insert(root)
+    assert tree == {{nil, {4, 1, 0}, nil}, {5, 1, -1}, nil}
+
+    root = BBTree.tree(5)
+    tree = 6 |> BBTree.insert(root)
+    assert tree == {nil, {5, 1, 1}, {nil, {6, 1, 0}, nil}}
+  end
+  test "lesser and greater than inserts keep tree balanced" do
+    tree = [4, 6] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    assert tree == {
+      {nil, {4, 1, 0}, nil}, {5, 1, 0}, {nil, {6, 1, 0}, nil}
+    }
+  end
+
+  test "2 lesser inserts" do
+    tree1 = [4, 3] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    tree2 = [3, 4] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    assert tree1 == tree2
+    assert tree1 == {
+      {nil, {3, 1, 0}, nil}, {4, 1, 0}, {nil, {5, 1, 0}, nil}
+    }
+  end
+  test "2 greater inserts" do
+    tree1 = [6, 7] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    tree2 = [7, 6] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    assert tree1 == tree2
+    assert tree1 == {
+      {nil, {5, 1, 0}, nil}, {6, 1, 0}, {nil, {7, 1, 0}, nil}
+    }
+  end
+
+  test "random tree #1: [3, 2, 6, 1, 9]" do
+    tree = [3, 2, 6, 1, 9] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    assert tree == {
+      {{nil, {1, 1, 0}, nil}, {2, 1, -1}, nil},
+      {3, 1, 1},
+      {{nil, {5, 1, 0}, nil}, {6, 1, 0}, {nil, {9, 1, 0}, nil}}
+    }
+  end
+
+  test "random tree #2: [2, 8, 5, 1, 6]" do
+    tree = [2, 8, 5, 1, 6] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    assert tree == {
+      {{nil, {1, 1, 0}, nil}, {2, 1, -1}, nil},
+      {5, 2, 0},
+      {{nil, {6, 1, 0}, nil}, {8, 1, -1}, nil}
+    }
+  end
+
+  test "random tree #3: [18, 14, 17, 20, 10, 10, 6, 9, 20, 8]" do
+#    tree = [18, 14, 17, 20, 10, 10, 6, 9, 20, 8] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    tree = [18, 14, 17, 20, 10, 6, 9, 8] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    assert tree == {
+      {
+        {nil, {5, 1, 0}, nil},
+        {6, 1, 2},
+        {{nil, {8, 1, 0}, nil}, {9, 1, 0}, {nil, {10, 1, 0}, nil}}
+      },
+      {14, 1, -2},
+      {{nil, {17, 1, 0}, nil}, {18, 1, 0}, {nil, {20, 1, 0}, nil}}
+    }
+  end
+
+  test "random tree #4: [5, 2, 4, 17, 3, 14, 2, 16, 19, 6]" do
+    tree = [5, 2, 4, 17, 3, 14, 2, 16, 19, 6] |> Enum.reduce(BBTree.tree(5), &BBTree.insert/2)
+    assert tree == {
+      {},
+      {},
+      {}
+    }
   end
 end
