a721560059ad1b89131a94da53c2bf513fb493f1
czrpb <czrpb00@gmail.com>
2021-02-01 16:59:06 -0800

    init

---
+++ ds/qtree/lib/qtree.ex
@@ -0,0 +1,18 @@
+defmodule Qtree do
+  @moduledoc """
+  Documentation for `Qtree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Qtree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ds/qtree/test/qtree_test.exs
@@ -0,0 +1,8 @@
+defmodule QtreeTest do
+  use ExUnit.Case
+  doctest Qtree
+
+  test "greets the world" do
+    assert Qtree.hello() == :world
+  end
+end
