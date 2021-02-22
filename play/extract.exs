4bec047717858a14704ad55e0d512c3a3b57fc6b
czrpb <czrpb00@gmail.com>
2021-02-21 23:18:50 -0800

    extracting sublists

---
+++ play/extract.exs
@@ -0,0 +1,43 @@
+defmodule Extract do
+  def extract(list, stop, start) do
+    list
+    |> Enum.reverse
+    |> Enum.reduce_while(
+      {:error, {false, []}},
+      fn w, {_code, {capture, captured}}=acc ->
+#        {w, acc} |> inspect |> IO.puts
+
+        return = cond do
+          stop.(w) and capture -> {:halt, {:ok, [w|captured]}}
+          start.(w) or capture -> {:cont, {:error, {true, [w|captured]}}}
+          true -> {:cont, acc}
+        end
+
+#        return |> inspect |> IO.puts
+        return
+      end
+    )
+  end
+end
+
+
+input = ~W<this is a test to see if i can figure out how to extract the text i want easily>
+expected1 = ~W<can figure out how to extract the text>
+expected2 = ~W<if i can figure out how to extract the text>
+
+#result = input |> Extract.extract(["if", "can"], ["text"])
+result = input |> Extract.extract(&Regex.match?(~R[^iff|caan$], &1), &Regex.match?(~R[^text$], &1))
+
+#result |> inspect |> IO.puts
+
+status = case result do
+  {:ok, result} -> result in [expected1, expected2]
+  {:error, {_, partial}} -> partial
+end
+
+case status do
+  true -> IO.puts("PASSED")
+  [] -> IO.puts("FAILED to find ending RE (dont know about starting)")
+  status -> IO.puts("FAILED to find starting RE: Got #{status |> inspect}")
+end
+7
