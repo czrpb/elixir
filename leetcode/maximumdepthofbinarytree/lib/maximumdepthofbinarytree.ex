9ede07682501114b24cbbc674b8fa23cf8b3e54e
czrpb <czrpb00@gmail.com>
2021-05-10 13:02:15 -0700

    leetcode: maximumdepthofbinarytree

---
+++ leetcode/maximumdepthofbinarytree/lib/maximumdepthofbinarytree.ex
@@ -0,0 +1,45 @@
+defmodule MaximumDepthOfBinaryTree do
+  @moduledoc """
+  Documentation for `MaximumDepthOfBinaryTree`.
+  """
+
+  def hello do
+    :world
+  end
+
+  defp _depth_rec(nil) do
+    0
+  end
+  defp _depth_rec({left, _value, right}) do
+    leftdepth = _depth_rec(left)
+    rightdepth = _depth_rec(right)
+
+    max(leftdepth, rightdepth) + 1
+  end
+  def depth_via_recursion(tree) do
+    _depth_rec(tree)
+  end
+
+
+  defp _depth_iter(nodes, count \\ 0)
+  defp _depth_iter([], count) do
+    count
+  end
+  defp _depth_iter(nodes, count) do
+    subtrees = nodes
+    |> Enum.flat_map(
+      fn tree ->
+        case tree do
+          {nil, _, nil}    -> []
+          {nil, _, right}  -> [right]
+          {left, _, nil}   -> [left]
+          {left, _, right} -> [left, right]
+        end
+      end
+    )
+    _depth_iter(subtrees, count+1)
+  end
+  def depth_via_iteration(tree) do
+    _depth_iter(tree)
+  end
+end
---
+++ leetcode/maximumdepthofbinarytree/test/maximumdepthofbinarytree_test.exs
@@ -0,0 +1,101 @@
+defmodule MaximumDepthOfBinaryTreeTest do
+  use ExUnit.Case
+  doctest MaximumDepthOfBinaryTree
+
+  test "greets the world" do
+    assert MaximumDepthOfBinaryTree.hello() == :world
+  end
+
+  describe "recursive solution" do
+
+    test "empty/nil tree" do
+      tree = nil
+      assert MaximumDepthOfBinaryTree.depth_via_recursion(tree) == 0
+    end
+
+    test "root only tree" do
+      tree = {nil, :value, nil}
+      assert MaximumDepthOfBinaryTree.depth_via_recursion(tree) == 1
+    end
+
+    test "left leaning" do
+      tree = {
+        {
+          {nil, :value, nil},
+          :value,
+          nil
+        },
+        :value,
+        nil
+      }
+      assert MaximumDepthOfBinaryTree.depth_via_recursion(tree) == 3
+    end
+    test "right leaning" do
+      tree = {
+        nil,
+        :value,
+        {
+          {nil, :value, nil},
+          :value,
+          nil
+        },
+      }
+      assert MaximumDepthOfBinaryTree.depth_via_recursion(tree) == 3
+    end
+    test "balanced" do
+      tree = {
+        {nil, :value, nil},
+        :value,
+        {nil, :value, nil}
+      }
+      assert MaximumDepthOfBinaryTree.depth_via_recursion(tree) == 2
+    end
+  end
+
+  describe "iterative solution" do
+
+    test "empty/nil tree" do
+      tree = []
+      assert MaximumDepthOfBinaryTree.depth_via_iteration(tree) == 0
+    end
+
+    test "root only tree" do
+      tree = [{nil, :value, nil}]
+      assert MaximumDepthOfBinaryTree.depth_via_iteration(tree) == 1
+    end
+
+    test "left leaning" do
+      tree = [{
+        {
+          {nil, :value, nil},
+          :value,
+          nil
+        },
+        :value,
+        nil
+      }]
+      assert MaximumDepthOfBinaryTree.depth_via_iteration(tree) == 3
+    end
+    test "right leaning" do
+      tree = [{
+        nil,
+        :value,
+        {
+          {nil, :value, nil},
+          :value,
+          nil
+        },
+      }]
+      assert MaximumDepthOfBinaryTree.depth_via_iteration(tree) == 3
+    end
+    test "balanced" do
+      tree = [{
+        {nil, :value, nil},
+        :value,
+        {nil, :value, nil}
+      }]
+      assert MaximumDepthOfBinaryTree.depth_via_iteration(tree) == 2
+    end
+  end
+
+end
