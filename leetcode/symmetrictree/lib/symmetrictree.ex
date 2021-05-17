2ab5c5b15dd1e56b7824ce3abac96651ff10d581
czrpb <czrpb00@gmail.com>
2021-05-16 19:45:37 -0700

    leetcode: iterative version

--- leetcode/symmetrictree/lib/symmetrictree.ex
+++ leetcode/symmetrictree/lib/symmetrictree.ex
@@ -11,7 +11,7 @@ defmodule SymmetricTree do
     to_list(left) ++ [value] ++ to_list(right)
   end
 
-  def tree({lefttree, root, righttree}) do
+  def tree({lefttree, _root, righttree}) do
     left = to_list(lefttree)
     right = to_list(righttree) |> Enum.reverse
 
@@ -19,6 +19,40 @@ defmodule SymmetricTree do
       :symmetric
     else
       :not_symmetric
+    end
+  end
+
+  defp tree_iter_({[], []}) do
+    {:ok, :symmetric}
+  end
+  defp tree_iter_({lefts, rights}) do
+    #IO.puts("#{lefts|>inspect} | #{rights|>inspect}")
+    if Enum.count(lefts) != Enum.count(rights) do
+      {:err, :not_symmetric, "differing subtree size"}
+    else
+      [lefts, rights]
+      |> Enum.zip
+      |> Enum.reduce(
+        {[], []},
+        fn
+          {nil, nil}, acc -> acc
+          {{ll, v, lr}, {rl, v, rr}}, {new_lefts, new_rights} ->
+            {[lr, ll|new_lefts], [rl, rr|new_rights]}
+        end
+      )
+      |> tree_iter_
+    end
+  end
+  def tree_iter(root) do
+    if root == nil do
+      {:ok, :symmetric}
+    else
+      #try do
+        {{ll, v, lr}, _, {rl, v, rr}} = root
+        tree_iter_({[lr, ll], [rl, rr]})
+      #rescue
+      #  err -> {:err, :not_symmetric, err}
+      #end
     end
   end
 end
--- leetcode/symmetrictree/test/symmetrictree_test.exs
+++ leetcode/symmetrictree/test/symmetrictree_test.exs
@@ -48,4 +48,194 @@ defmodule SymmetricTreeTest do
     }
     assert SymmetricTree.tree(tree) == :not_symmetric
   end
+
+  test "1 level: symmetric (iterative)" do
+    tree = {{nil, :child, nil}, :parent, {nil, :child, nil}}
+    assert SymmetricTree.tree_iter(tree) == {:ok, :symmetric}
+  end
+
+  test "leetcode test 1 (iterative)" do
+    tree = {
+      {
+        {nil, 3, nil},
+        2,
+        {nil, 4, nil}
+      },
+      1,
+      {
+        {nil, 4, nil},
+        2,
+        {nil, 3, nil}
+      }
+    }
+    assert SymmetricTree.tree_iter(tree) == {:ok, :symmetric}
+  end
+  test "leetcode test 1.1 (iterative)" do
+    tree = {
+      {
+        {nil, 3, nil},
+        2,
+        {nil, 4, nil}
+      },
+      1,
+      {
+        {nil, 5, nil},
+        2,
+        {nil, 3, nil}
+      }
+    }
+    assert SymmetricTree.tree_iter(tree) == {:ok, :symmetric}
+  end
+
+  test "leetcode test 2 (iterative)" do
+    tree = {
+      {
+        nil,
+        2,
+        {nil, 3.1, nil}
+      },
+      1,
+      {
+        nil,
+        2,
+        {nil, 3.2, nil}
+      }
+    }
+    {:err, :not_symmetric, exception} = SymmetricTree.tree_iter(tree)
+    exception|>inspect|>IO.puts
+    assert True
+  end
+
+  test "deeper tree (iterative)" do
+    tree = {
+      {
+        {
+          {
+            {nil, 9, nil},
+            5,
+            {nil, 10, nil}
+          },
+          3,
+          {
+            {nil, 11, nil},
+            6,
+            {nil, 12, nil}
+          }
+        },
+        2,
+        {
+          {
+            {nil, 13, nil},
+            7,
+            {nil, 14, nil}
+          },
+          4,
+          {
+            {nil, 15, nil},
+            8,
+            {nil, 16, nil}
+          }
+        }
+      },
+      1,
+      {
+        {
+          {
+            {nil, 16, nil},
+            8,
+            {nil, 15, nil}
+          },
+          4,
+          {
+            {nil, 14, nil},
+            7,
+            {nil, 13, nil}
+          }
+        },
+        2,
+        {
+          {
+            {nil, 12, nil},
+            6,
+            {nil, 11, nil}
+          },
+          3,
+          {
+            {nil, 10, nil},
+            5,
+            {nil, 9, nil}
+          }
+        }
+      }
+    }
+    {:ok, :symmetric} = SymmetricTree.tree_iter(tree)
+    assert True
+  end
+
+  test "deeper tree but bad (iterative)" do
+    tree = {
+      {
+        {
+          {
+            {nil, 9, nil},
+            5,
+            {nil, 10, nil}
+          },
+          3,
+          {
+            {nil, 11, {nil, 17, nil}},
+            6,
+            {nil, 12, nil}
+          }
+        },
+        2,
+        {
+          {
+            {nil, 13, {nil, 18, nil}},
+            7,
+            {nil, 14, nil}
+          },
+          4,
+          {
+            {nil, 15, nil},
+            8,
+            {nil, 16, nil}
+          }
+        }
+      },
+      1,
+      {
+        {
+          {
+            {nil, 16, nil},
+            8,
+            {nil, 15, nil}
+          },
+          4,
+          {
+            {nil, 14, nil},
+            7,
+            {nil, 13, {nil, 18, nil}}
+          }
+        },
+        2,
+        {
+          {
+            {nil, 12, nil},
+            6,
+            {{nil, 17, nil}, 11, nil}
+          },
+          3,
+          {
+            {nil, 10, nil},
+            5,
+            {nil, 9, nil}
+          }
+        }
+      }
+    }
+    {:ok, :symmetric} = SymmetricTree.tree_iter(tree)
+    assert True
+  end
+
 end
