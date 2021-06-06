eb13c138e6aaf399daad2d354013c34257425190
czrpb <czrpb00@gmail.com>
2021-06-06 16:27:26 -0700

    tree updates

--- ds/bbtreefromsortedlist/lib/bbtreefromsortedlist.ex
+++ ds/bbtreefromsortedlist/lib/bbtreefromsortedlist.ex
@@ -5,5 +5,36 @@ defmodule BBTreeFromSortedList do
 
   def treenode(value), do: {nil, value, nil}
 
-  
+  def insert(newvalue, nil), do: treenode(newvalue)
+
+  def insert(newvalue, {left, value, right})
+  when newvalue < value
+  do
+    {insert(newvalue, left), value, right}
+  end
+
+  def insert(newvalue, {left, value, right})
+  when newvalue > value
+  do
+    {left, value, insert(newvalue, right)}
+  end
+
+  def new(values) do
+    [root|values] = values
+    values |> Enum.reduce(treenode(root), &insert/2)
+  end
+
+  def from_sorted([]), do: nil
+  def from_sorted(values) do
+    {lvalues, [root|rvalues]} = values
+    |> Enum.split(div(Enum.count(values), 2))
+
+    {from_sorted(lvalues), root, from_sorted(rvalues)}
+
+
+    # Enum.zip(Enum.reverse(lvalues), rvalues)
+    # |> Enum.flat_map(&Tuple.to_list/1)
+    # |> (fn l -> [root|l] end).()
+    # |> new
+  end
 end
--- ds/bbtreefromsortedlist/test/bbtreefromsortedlist_test.exs
+++ ds/bbtreefromsortedlist/test/bbtreefromsortedlist_test.exs
@@ -2,7 +2,27 @@ defmodule BBTreeFromSortedListTest do
   use ExUnit.Case
   doctest BBTreeFromSortedList
 
-  test "greets the world" do
-    assert BBTreeFromSortedList.hello() == :world
+  test "[1,2,3]" do
+    input = 1..3 |> Enum.to_list
+    expected = {{nil, 1, nil}, 2, {nil, 3, nil}}
+    assert BBTreeFromSortedList.from_sorted(input) == expected
+  end
+
+  test "[1,2,3,4,5]" do
+    input = 1..5 |> Enum.to_list
+    expected = {{nil, 1, nil}, 2, {nil, 3, nil}}
+    assert BBTreeFromSortedList.from_sorted(input) == expected
+  end
+
+  test "[1,2,3,4,5,6,7]" do
+    input = 1..7 |> Enum.to_list
+    expected = {{nil, 1, nil}, 2, {nil, 3, nil}}
+    assert BBTreeFromSortedList.from_sorted(input) == expected
+  end
+
+  test "[1,2,3,4,5,6,7,8,9]" do
+    input = 1..9 |> Enum.to_list
+    expected = {{nil, 1, nil}, 2, {nil, 3, nil}}
+    assert BBTreeFromSortedList.from_sorted(input) == expected
   end
 end
