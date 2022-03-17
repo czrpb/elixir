14a4ba99f315cae27c23833ca5d92490b5fa9af5
czrpb <czrpb@yahoo.com>
2022-03-17 12:44:46 -0700

    updates

--- play/sublist/lib/sublist.ex
+++ play/sublist/lib/sublist.ex
@@ -1,28 +1,70 @@
-defmodule Sublist do
+defmodule SubList1 do
   @moduledoc """
-  Documentation for `Sublist`.
+  Documentation for `SubList1`.
   """
 
   def sublist(l1, l2) do
     cond do
-      length(l1) == length(l2) -> sublist(l1, l2, :equal, l1)
-      length(l1) < length(l2) -> sublist(l1, l2, :sublist, l1)
-      true -> sublist(l2, l1, :superlist, l2)
+      length(l1) == length(l2) -> sublist(l1, l2, :equal, l1, [:ignore])
+      length(l1) < length(l2) -> sublist(l1, l2, :sublist, l1, [:ignore])
+      true -> sublist(l2, l1, :superlist, l2, [:ignore])
     end
   end
 
-  defp sublist([], [], result, _original), do: result
+  defp sublist([], [], result, _original, _matches), do: result
 
-  defp sublist([], _lt2, result, _original), do: result
+  defp sublist([], _lt2, result, _original, _matches), do: result
 
-  defp sublist(_lt1, [], _result, _original), do: :unequal
+  defp sublist(_lt1, [], _result, _original, _matches), do: :unequal
 
-  defp sublist([a | lt1], [a | lt2], result, original) do
-    sublist(lt1, lt2, result, original)
+  defp sublist([a | lt1], [a | lt2], result, original, matches) do
+    sublist(lt1, lt2, result, original, [a | matches])
   end
 
-  defp sublist([_a | _], [_b  | lt2], result, original) do
-    sublist(original, lt2, result, original)
+  defp sublist([_a | _], [_b | lt2], result, original, matches) do
+    # matches |> inspect |> IO.puts
+    [_ | matches] = Enum.reverse(matches)
+    sublist(original, matches ++ lt2, result, original, [:ignore])
+  end
+end
+
+defmodule SubList2 do
+  def sublist(l1, l2) do
+    l1_l = length(l1)
+    l2_l = length(l2)
+
+    cond do
+      l1_l == l2_l -> if l1 == l2, do: :equal, else: :unequal
+      l1_l < l2_l and l1 in Enum.chunk_every(l2, l1_l, 1) -> :sublist
+      l2 in Enum.chunk_every(l1, l2_l, 1) -> :superlist
+      true -> :unequal
+    end
+  end
+end
+
+defmodule SubList3 do
+  @type listtype :: :unequal | :sublist | :superlist | :equal
+
+  @spec sublist(list, list) :: listtype
+  def sublist(l1, l2) do
+    l1_l = length(l1)
+    l2_l = length(l2)
+
+    cond do
+      l1_l == l2_l -> if l1 == l2, do: :equal, else: :unequal
+      l1_l < l2_l -> sublist(l1, l2, :sublist)
+      true -> sublist(l2, l1, :superlist)
+    end
   end
 
+  @spec sublist(list, list, listtype) :: listtype
+  def sublist(_, [], _), do: :unequal
+
+  def sublist(l1, [_ | l2_tl] = l2, result) do
+    if List.starts_with?(l2, l1) do
+      result
+    else
+      sublist(l1, l2_tl, result)
+    end
+  end
 end
--- play/sublist/test/sublist_test.exs
+++ play/sublist/test/sublist_test.exs
@@ -1,60 +1,106 @@
-defmodule SublistTest do
+defmodule SubList1Test do
   use ExUnit.Case
-  doctest Sublist
+  doctest SubList1
 
   test "sublist([], [])" do
-    assert Sublist.sublist([], []) == :equal
+    assert SubList1.sublist([], []) == :equal
+    assert SubList2.sublist([], []) == :equal
+    assert SubList3.sublist([], []) == :equal
   end
 
   test "sublist([1], [1])" do
-    assert Sublist.sublist([1], [1]) == :equal
+    assert SubList1.sublist([1], [1]) == :equal
+    assert SubList2.sublist([1], [1]) == :equal
+    assert SubList3.sublist([1], [1]) == :equal
   end
 
   test "sublist([1, 2, 3], [1, 2, 3])" do
-    assert Sublist.sublist([1, 2, 3], [1, 2, 3]) == :equal
+    assert SubList1.sublist([1, 2, 3], [1, 2, 3]) == :equal
+    assert SubList2.sublist([1, 2, 3], [1, 2, 3]) == :equal
+    assert SubList3.sublist([1, 2, 3], [1, 2, 3]) == :equal
   end
 
   test "sublist([1], [2])" do
-    assert Sublist.sublist([1], [2]) == :unequal
+    assert SubList1.sublist([1], [2]) == :unequal
+    assert SubList2.sublist([1], [2]) == :unequal
+    assert SubList3.sublist([1], [2]) == :unequal
   end
 
   test "sublist([1], [1, 2])" do
-    assert Sublist.sublist([1], [1, 2]) == :sublist
+    assert SubList1.sublist([1], [1, 2]) == :sublist
+    assert SubList2.sublist([1], [1, 2]) == :sublist
+    assert SubList3.sublist([1], [1, 2]) == :sublist
   end
 
   test "sublist([2], [1, 2])" do
-    assert Sublist.sublist([2], [1, 2]) == :sublist
+    assert SubList1.sublist([2], [1, 2]) == :sublist
+    assert SubList2.sublist([2], [1, 2]) == :sublist
+    assert SubList3.sublist([2], [1, 2]) == :sublist
   end
 
   test "sublist([1, 2], [1])" do
-    assert Sublist.sublist([1, 2], [1]) == :superlist
+    assert SubList1.sublist([1, 2], [1]) == :superlist
+    assert SubList2.sublist([1, 2], [1]) == :superlist
+    assert SubList3.sublist([1, 2], [1]) == :superlist
   end
 
   test "sublist([1, 2], [2])" do
-    assert Sublist.sublist([1, 2], [2]) == :superlist
+    assert SubList1.sublist([1, 2], [2]) == :superlist
+    assert SubList2.sublist([1, 2], [2]) == :superlist
+    assert SubList3.sublist([1, 2], [2]) == :superlist
   end
 
   test "sublist([1, 2, 3], [1, 5, 3])" do
-    assert Sublist.sublist([1, 2, 3], [1, 5, 3]) == :unequal
+    assert SubList1.sublist([1, 2, 3], [1, 5, 3]) == :unequal
+    assert SubList2.sublist([1, 2, 3], [1, 5, 3]) == :unequal
+    assert SubList3.sublist([1, 2, 3], [1, 5, 3]) == :unequal
   end
 
   test "sublist([2], [1, 2, 3])" do
-    assert Sublist.sublist([2], [1, 2, 3]) == :sublist
+    assert SubList1.sublist([2], [1, 2, 3]) == :sublist
+    assert SubList2.sublist([2], [1, 2, 3]) == :sublist
+    assert SubList3.sublist([2], [1, 2, 3]) == :sublist
   end
 
   test "sublist([1, 2, 4], [1, 2, 3, 4, 5])" do
-    assert Sublist.sublist([1, 2, 4], [1, 2, 3, 4, 5]) == :unequal
+    assert SubList1.sublist([1, 2, 4], [1, 2, 3, 4, 5]) == :unequal
+    assert SubList2.sublist([1, 2, 4], [1, 2, 3, 4, 5]) == :unequal
+    assert SubList3.sublist([1, 2, 4], [1, 2, 3, 4, 5]) == :unequal
   end
 
   test "sublist([1, 2, 3], [1, 2, 3, 4, 5])" do
-    assert Sublist.sublist([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
+    assert SubList1.sublist([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
+    assert SubList2.sublist([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
+    assert SubList3.sublist([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
   end
 
   test "sublist([3, 4, 5], [1, 2, 3, 4, 5])" do
-    assert Sublist.sublist([3, 4, 5], [1, 2, 3, 4, 5]) == :sublist
+    assert SubList1.sublist([3, 4, 5], [1, 2, 3, 4, 5]) == :sublist
+    assert SubList2.sublist([3, 4, 5], [1, 2, 3, 4, 5]) == :sublist
+    assert SubList3.sublist([3, 4, 5], [1, 2, 3, 4, 5]) == :sublist
   end
 
   test "sublist([2, 3, 4], [1, 2, 3, 4, 5])" do
-    assert Sublist.sublist([2, 3, 4], [1, 2, 3, 4, 5]) == :sublist
+    assert SubList1.sublist([2, 3, 4], [1, 2, 3, 4, 5]) == :sublist
+    assert SubList2.sublist([2, 3, 4], [1, 2, 3, 4, 5]) == :sublist
+    assert SubList3.sublist([2, 3, 4], [1, 2, 3, 4, 5]) == :sublist
+  end
+
+  test "sublist([1, 1, 2], [1, 2, 1, 1, 2])" do
+    assert SubList1.sublist([1, 1, 2], [1, 2, 1, 1, 2]) == :sublist
+    assert SubList2.sublist([1, 1, 2], [1, 2, 1, 1, 2]) == :sublist
+    assert SubList3.sublist([1, 1, 2], [1, 2, 1, 1, 2]) == :sublist
+  end
+
+  test "sublist([1, 1, 2], [1, 1, 1, 2])" do
+    assert SubList1.sublist([1, 1, 2], [1, 1, 1, 2]) == :sublist
+    assert SubList2.sublist([1, 1, 2], [1, 1, 1, 2]) == :sublist
+    assert SubList3.sublist([1, 1, 2], [1, 1, 1, 2]) == :sublist
+  end
+
+  test "sublist([1, 2, 4], [1, 2, 3, 4])" do
+    assert SubList1.sublist([1, 2, 4], [1, 2, 3, 4]) == :unequal
+    assert SubList2.sublist([1, 2, 4], [1, 2, 3, 4]) == :unequal
+    assert SubList3.sublist([1, 2, 4], [1, 2, 3, 4]) == :unequal
   end
 end
