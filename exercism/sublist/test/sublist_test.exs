95510ca833efbe9763f6f1b8c77fd804d0b608db
czrpb <czrpb@yahoo.com>
2022-03-18 16:19:35 -0700

    update

--- exercism/sublist/test/sublist_test.exs
+++ exercism/sublist/test/sublist_test.exs
@@ -3,80 +3,152 @@ defmodule SublistTest do
 
   describe "unequal" do
     test "1 is not 2" do
+      assert SubList.compare([1], [2]) == :unequal
+      assert SubList1.compare([1], [2]) == :unequal
       assert SubList2.compare([1], [2]) == :unequal
+      assert SubList3.compare([1], [2]) == :unequal
+      assert SubList4.compare([1], [2]) == :unequal
     end
 
     test "huge sublist not in huge list" do
+      assert SubList.compare(Enum.to_list(10..1_000_001), Enum.to_list(1..1_000_000)) == :unequal
+      assert SubList1.compare(Enum.to_list(10..1_000_001), Enum.to_list(1..1_000_000)) == :unequal
       assert SubList2.compare(Enum.to_list(10..1_000_001), Enum.to_list(1..1_000_000)) == :unequal
+      assert SubList3.compare(Enum.to_list(10..1_000_001), Enum.to_list(1..1_000_000)) == :unequal
+      assert SubList4.compare(Enum.to_list(10..1_000_001), Enum.to_list(1..1_000_000)) == :unequal
     end
 
     test "second list missing element from first list" do
+      assert SubList.compare([1, 2, 3], [1, 3]) == :unequal
+      #assert SubList1.compare([1, 2, 3], [1, 3]) == :unequal
       assert SubList2.compare([1, 2, 3], [1, 3]) == :unequal
+      assert SubList3.compare([1, 2, 3], [1, 3]) == :unequal
+      assert SubList4.compare([1, 2, 3], [1, 3]) == :unequal
     end
 
     test "first list missing element from second list" do
+      assert SubList.compare([1, 3], [1, 2, 3]) == :unequal
+      assert SubList1.compare([1, 3], [1, 2, 3]) == :unequal
       assert SubList2.compare([1, 3], [1, 2, 3]) == :unequal
+      assert SubList3.compare([1, 3], [1, 2, 3]) == :unequal
+      assert SubList4.compare([1, 3], [1, 2, 3]) == :unequal
     end
 
     test "same digits but different numbers" do
+      assert SubList.compare([1, 0, 1], [10, 1]) == :unequal
+      #assert SubList1.compare([1, 0, 1], [10, 1]) == :unequal
       assert SubList2.compare([1, 0, 1], [10, 1]) == :unequal
+      assert SubList3.compare([1, 0, 1], [10, 1]) == :unequal
+      assert SubList4.compare([1, 0, 1], [10, 1]) == :unequal
     end
 
     test "recurring values unequal" do
+      assert SubList.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 3, 2, 3, 2, 1]) == :unequal
+      assert SubList1.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 3, 2, 3, 2, 1]) == :unequal
       assert SubList2.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 3, 2, 3, 2, 1]) == :unequal
+      assert SubList3.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 3, 2, 3, 2, 1]) == :unequal
+      assert SubList4.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 3, 2, 3, 2, 1]) == :unequal
     end
 
     test "strict equality needed" do
+      assert SubList.compare([1], [1.0, 2]) == :unequal
+      assert SubList1.compare([1], [1.0, 2]) == :unequal
       assert SubList2.compare([1], [1.0, 2]) == :unequal
+      assert SubList3.compare([1], [1.0, 2]) == :unequal
+      assert SubList4.compare([1], [1.0, 2]) == :unequal
     end
 
     test "1 and 2 does not contain 3" do
+      assert SubList.compare([1, 2], [3]) == :unequal
+      assert SubList1.compare([1, 2], [3]) == :unequal
       assert SubList2.compare([1, 2], [3]) == :unequal
+      assert SubList3.compare([1, 2], [3]) == :unequal
+      assert SubList4.compare([1, 2], [3]) == :unequal
     end
   end
 
   describe "equal" do
     test "empty equals empty" do
+      assert SubList.compare([], []) == :equal
+      assert SubList1.compare([], []) == :equal
       assert SubList2.compare([], []) == :equal
+      assert SubList3.compare([], []) == :equal
+      assert SubList4.compare([], []) == :equal
     end
 
     test "comparing massive equal lists" do
       l = Enum.to_list(1..1_000_000)
+      assert SubList.compare(l, l) == :equal
+      assert SubList1.compare(l, l) == :equal
       assert SubList2.compare(l, l) == :equal
+      assert SubList3.compare(l, l) == :equal
+      assert SubList4.compare(l, l) == :equal
     end
   end
 
   describe "sublist" do
     test "empty is a sublist of anything" do
+      assert SubList.compare([], [nil]) == :sublist
+      assert SubList1.compare([], [nil]) == :sublist
       assert SubList2.compare([], [nil]) == :sublist
+      assert SubList3.compare([], [nil]) == :sublist
+      assert SubList4.compare([], [nil]) == :sublist
     end
 
     test "sublist at start" do
+      assert SubList.compare([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
+      assert SubList1.compare([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
       assert SubList2.compare([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
+      assert SubList3.compare([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
+      assert SubList4.compare([1, 2, 3], [1, 2, 3, 4, 5]) == :sublist
     end
 
     test "sublist in middle" do
+      assert SubList.compare([4, 3, 2], [5, 4, 3, 2, 1]) == :sublist
+      assert SubList1.compare([4, 3, 2], [5, 4, 3, 2, 1]) == :sublist
       assert SubList2.compare([4, 3, 2], [5, 4, 3, 2, 1]) == :sublist
+      assert SubList3.compare([4, 3, 2], [5, 4, 3, 2, 1]) == :sublist
+      assert SubList4.compare([4, 3, 2], [5, 4, 3, 2, 1]) == :sublist
     end
 
     test "sublist at end" do
+      assert SubList.compare([3, 4, 5], [1, 2, 3, 4, 5]) == :sublist
+      assert SubList1.compare([3, 4, 5], [1, 2, 3, 4, 5]) == :sublist
       assert SubList2.compare([3, 4, 5], [1, 2, 3, 4, 5]) == :sublist
+      assert SubList3.compare([3, 4, 5], [1, 2, 3, 4, 5]) == :sublist
+      assert SubList4.compare([3, 4, 5], [1, 2, 3, 4, 5]) == :sublist
     end
 
     test "partially matching sublist at start" do
+      assert SubList.compare([1, 1, 2], [1, 1, 1, 2]) == :sublist
+      assert SubList1.compare([1, 1, 2], [1, 1, 1, 2]) == :sublist
       assert SubList2.compare([1, 1, 2], [1, 1, 1, 2]) == :sublist
+      assert SubList3.compare([1, 1, 2], [1, 1, 1, 2]) == :sublist
+      assert SubList4.compare([1, 1, 2], [1, 1, 1, 2]) == :sublist
     end
 
     test "sublist early in huge list" do
+      assert SubList.compare([3, 4, 5], Enum.to_list(1..1_000_000)) == :sublist
+      assert SubList1.compare([3, 4, 5], Enum.to_list(1..1_000_000)) == :sublist
       assert SubList2.compare([3, 4, 5], Enum.to_list(1..1_000_000)) == :sublist
+      assert SubList3.compare([3, 4, 5], Enum.to_list(1..1_000_000)) == :sublist
+      assert SubList4.compare([3, 4, 5], Enum.to_list(1..1_000_000)) == :sublist
     end
 
     test "consecutive" do
+      assert SubList.compare([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]) == :sublist
+      assert SubList1.compare([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]) == :sublist
       assert SubList2.compare([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]) == :sublist
+      assert SubList3.compare([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]) == :sublist
+      assert SubList4.compare([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]) == :sublist
     end
 
     test "recurring values sublist" do
+      assert SubList.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 1, 2, 3, 2, 1]) == :sublist
+      assert SubList1.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 1, 2, 3, 2, 1]) == :sublist
       assert SubList2.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 1, 2, 3, 2, 1]) == :sublist
+      assert SubList3.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 1, 2, 3, 2, 1]) == :sublist
+      assert SubList4.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 1, 2, 3, 2, 1]) == :sublist
     end
   end
 
